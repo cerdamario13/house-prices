81e4133d3953ea17bb114d984701b35e44025020
cerdamario13 <cerdamario13@gmail.com>
2024-07-12 06:40:49 -0500

    added custom chart component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import './App.css';
 import { CommandBar, ICommandBarItemProps } from '@fluentui/react';
 import { initializeIcons } from '@fluentui/font-icons-mdl2';
-import { IChartProps, LineChart } from '@fluentui/react-charting';
 
 // Initialize icons in the app
 initializeIcons();
@@ -18,49 +17,10 @@ function App() {
     },
   ];
 
-  const data: IChartProps = {
-    chartTitle: 'Line Chart',
-    lineChartData: [
-      {
-        legend: 'Line 1',
-        data: [
-          {
-            x: 1910,
-            y: 2.4,
-          },
-          {
-            x: 1920,
-            y: 2.9,
-          },
-          {
-            x: 1950,
-            y: 3.5,
-          },
-        ]
-      }
-    ]
-  }
-
   return (
     <>
       <CommandBar
         items={commandBarItems}
-      />
-
-      <LineChart
-        // Force rerender when any of the following states change
-        key={`1234`}
-        // culture={window.navigator.language}
-        data={data}
-        // legendsOverflowText={'Overflow Items'}
-        // yMinValue={200}
-        // yMaxValue={301}
-        height={200}
-        width={100}
-        xAxisTickCount={10}
-        enablePerfOptimization={true}
-        yAxisTitle={'y title'}
-        xAxisTitle={'x title'}
       />
     </>
   );
