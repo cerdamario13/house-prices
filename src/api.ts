a485266251709d3748143b112187d3292358c185
cerdamario13 <cerdamario13@gmail.com>
2025-07-05 11:34:12 -0500

    Add year to year change fetch function

--- src/api.ts
+++ src/api.ts
@@ -29,7 +29,7 @@ export const getPriceIndexRatio = async (location: any): Promise<{[key: string]:
     location: location
   }
   const queryString = new URLSearchParams(params).toString();
-  const url = `${mainURL}/pi_ratio?${queryString}`
+  const url = `${mainURL}/pi_ratio?${queryString}`;
   try {
     const response = await fetch(url);
     if (!response.ok) {
@@ -40,5 +40,24 @@ export const getPriceIndexRatio = async (location: any): Promise<{[key: string]:
   } catch (e) {
     console.error('There was a problem with the fetch operation - PI Ratio');
     return {"Error": "Error fetching PI Ratio"};
+  }
+};
+
+export const getYTYChange = async (location: any): Promise<{[key: string]: any}> => {
+  const params = {
+    location: location
+  }
+  const queryString = new URLSearchParams(params).toString();
+  const url = `${mainURL}/yty_change?${queryString}`;
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      return {"Error": "Year to Year Fetch response was not ok"}
+    }
+    const data = await response.json();
+    return data;
+  } catch (e) {
+    console.error('There was a problem with the fetch operation - Year to Year Change');
+    return {"Error": "Error fetching year to year"};
   }
 };
