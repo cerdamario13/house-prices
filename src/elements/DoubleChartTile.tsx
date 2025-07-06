921e97f408d0c7c24cba360cb6945ff712fd7844
cerdamario13 <cerdamario13@gmail.com>
2025-07-06 11:24:15 -0500

    DoubleChartTile initial commit

---
+++ src/elements/DoubleChartTile.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+interface DoubleChartTitleProps {
+  data: any;
+};
+
+export const DoubleChartTitle: React.FC<DoubleChartTitleProps> = ({
+  data
+}) => {
+
+  return (
+    <>
+    </>
+  );
+
+};
